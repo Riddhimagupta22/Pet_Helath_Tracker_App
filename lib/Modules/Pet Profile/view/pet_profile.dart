e89491cd0f6de1a89c24c0b067c4689fe3f12d8e
Riddhima Gupta <griddhima226@gmail.com>
2025-07-16 23:04:52 +0530

    changes

--- lib/Modules/Pet Profile/view/pet_profile.dart
+++ lib/Modules/Pet Profile/view/pet_profile.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pet_app/Comman/widget/custom_field.dart';
+import 'package:pet_app/Controller/auth_controller.dart';
 import '../../../Controller/pet_controller.dart';
 
 class PetProfile extends StatefulWidget {
@@ -15,6 +16,7 @@ class PetProfile extends StatefulWidget {
 
 class _PetProfileState extends State<PetProfile> {
   final petController = Get.put(PetController());
+  final authController = Get.put(AuthController());
 
   @override
   void initState() {
@@ -55,7 +57,7 @@ class _PetProfileState extends State<PetProfile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: true,
+      resizeToAvoidBottomInset: false,
       extendBody: false,
       body: Stack(
         children: [
@@ -65,134 +67,140 @@ class _PetProfileState extends State<PetProfile> {
               fit: BoxFit.cover,
             ),
           ),
-          SingleChildScrollView(
-
-
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        IconButton(
-                            onPressed: () {},
-                            icon: Icon((Icons.arrow_back_ios_new))),
-                        Container(
-                          height: 40,
-                          width: 40,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15),
-                            color:
-                            Color.fromRGBO(243, 120, 29, 1).withOpacity(0.5),
-                          ),
-                          child: IconButton(
-                              onPressed: () {},
-                              icon: Center(child: Icon((Icons.logout_rounded),color: Colors.black,))),
-                        )
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 28.0),
-                      child: GestureDetector(
-                        onTap: _showImagePickerOptions,
-                        child: Obx(() {
-                          final imagePath = petController.imageUrl.value;
-
-                          return CircleAvatar(
-                            radius: 70,
-                            backgroundColor: Colors.white,
-                            backgroundImage: imagePath.isNotEmpty
-                                ? (imagePath.startsWith('http')
-                                    ? NetworkImage(imagePath)
-                                    : FileImage(File(imagePath)) as ImageProvider)
-                                : null,
-                            child: imagePath.isEmpty
-                                ? const Icon(Icons.pets,
-                                    size: 40, color: Colors.grey)
-                                : null,
-                          );
-                        }),
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon((Icons.arrow_back_ios_new))),
+                    Container(
+                      height: 40,
+                      width: 40,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Color.fromRGBO(243, 120, 29, 1).withOpacity(0.5),
                       ),
-                    ),
-                    const SizedBox(height: 15),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Container(
-                          child: Column(children: [
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            "Pet",
-                            style: GoogleFonts.poppins(
-                              fontWeight: FontWeight.w700,
-                              fontSize: 24,
-                              color: const Color.fromRGBO(79, 45, 39, 1),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 12),
-                        CustomField(
-                            controller: petController.nameController,
-                            label: 'Name'),
-                        CustomField(
-                            controller: petController.genderController,
-                            label: 'Gender'),
-                        const SizedBox(height: 10),
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            "Pet Details",
-                            style: GoogleFonts.poppins(
-                              fontWeight: FontWeight.w700,
-                              fontSize: 24,
-                              color: const Color.fromRGBO(79, 45, 39, 1),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 12),
-                        CustomField(
-                            controller: petController.weightController,
-                            label: 'Weight'),
-                        CustomField(
-                            controller: petController.ageController,
-                            label: 'Age'),
-                        const SizedBox(height: 12),
-                        SizedBox(
-                          height: 50,
-                          width: 154,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: Color.fromRGBO(243, 120, 29, 1),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(25),
-                              ),
-                            ),
-                            onPressed: () {
-                              petController.saveProfile();
-                            },
-                            child: Text(
-                              "Save",
-                              style: GoogleFonts.poppins(
-                                fontSize: 24,
-                                fontWeight: FontWeight.w600,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-
-                      ])),
+                      child: IconButton(
+                          onPressed: () {
+                            authController.Signout();
+                          },
+                          icon: Center(
+                              child: Icon(
+                            (Icons.logout_rounded),
+                            color: Colors.black,
+                          ))),
                     )
                   ],
                 ),
-              ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 28.0),
+                  child: GestureDetector(
+                    onTap: _showImagePickerOptions,
+                    child: Obx(() {
+                      final imagePath = petController.imageUrl.value;
+
+                      return CircleAvatar(
+                        radius: 70,
+                        backgroundColor: Colors.white,
+                        backgroundImage: imagePath.isNotEmpty
+                            ? (imagePath.startsWith('http')
+                                ? NetworkImage(imagePath)
+                                : FileImage(File(imagePath)) as ImageProvider)
+                            : null,
+                        child: imagePath.isEmpty
+                            ? const Icon(Icons.pets,
+                                size: 40, color: Colors.grey)
+                            : null,
+                      );
+                    }),
+                  ),
+                ),
+                const SizedBox(height: 15),
+                SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Container(
+                        child: Column(children: [
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Pet",
+                          style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 24,
+                            color: const Color.fromRGBO(79, 45, 39, 1),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 12),
+                      CustomField(
+                        keyboardType: TextInputType.text,
+                          controller: petController.nameController,
+                          label: 'Name'),
+                      CustomField(
+                          keyboardType: TextInputType.text,
+                          controller: petController.genderController,
+                          label: 'Gender'),
+                      const SizedBox(height: 10),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Pet Details",
+                          style: GoogleFonts.poppins(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 24,
+                            color: const Color.fromRGBO(79, 45, 39, 1),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 12),
+                      CustomField(
+                          keyboardType: TextInputType.numberWithOptions(decimal: true),
+                          controller: petController.weightController,
+                          label: 'Weight'),
+                      CustomField(
+                          keyboardType: TextInputType.number,
+
+                          controller: petController.ageController,
+                          label: 'Age'),
+                      const SizedBox(height: 12),
+                      SizedBox(
+                        height: 50,
+                        width: 154,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color.fromRGBO(243, 120, 29, 1),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25),
+                            ),
+                          ),
+                          onPressed: () {
+                            petController.saveProfile();
+                          },
+                          child: Text(
+                            "Save",
+                            style: GoogleFonts.poppins(
+                              fontSize: 24,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                    ])),
+                  ),
+                )
+              ],
             ),
+          ),
         ],
       ),
     );
